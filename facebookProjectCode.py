bbe1d2453c27158883f2a7a4f0ada2234187fc9a
Rceballos101 <31495200+Rceballos101@users.noreply.github.com>
2017-10-29 16:01:26 -0400

    fixed our list issue, all users now have a correct list for each user

--- facebookProjectCode.py
+++ facebookProjectCode.py
@@ -1,33 +1,47 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Oct 28 16:07:14 2017
 
-@author: user
-"""
 
 def facebook():
-    fp = open('C:/python/project/small_network_data.txt','r')
-    n = fp.readline()
+    fp = open('C:/python/project/small_network_data.txt','r')#location of test data
+    #convert to input when final
+    n = fp.readline() # number of users in network
     n = int(n)
-    network = []
+    network = [] #holds all users friends
     
     for i in range(n):
-        network.append([])
+        network.append([]) #creates a list of each user and there friends
     
-    friends = fp.readlines()
+    friends = fp.readlines() #list of each user and 1 of there friends
 
-    for y in range(len(friends)):
+    for y in range(len(friends)):#eliminates white space from each list
         friends[y] = friends[y].strip()
 
-    for j in friends:
-        group = j.split(' ')
-        lst = eval(group[0])
-        lst2 = eval(group[1])
+    users = []#list of users
+    
+
+    for j in friends:#creates list of users
+        group = j.split(' ')#splits row into 2 friends     
+        for k in group:
+            if k not in users:
+                users.append(k)
+    print('Our users are ',users)
+        
+        
+    for f in friends:#inputs user followed by his friends in a list
+        group = f.split(' ')#splits row into 2 friends
+        f1 = eval(group[0])
+        f2 = eval(group[1])
+        if group[0] not in network[f1]:
+            network[f1].append(group[0])
+        if group[1] not in network[f2]:   
+            network[f2].append(group[1])
+        if group[0] not in network[f2]:
+            network[f2].append(group[0])
+        lst = eval(group[0])#user
         network[lst].append(group[1])
-        network[lst2].append(group[0])
+        if group[0] not in network[lst]:
+            network[lst].append(group[0])
+        
                 
-    print(network)
+    print(network)#testing purposes only
     fp.close()
 facebook()
-
-
